134f872c3c66e485e1f1c556f62537504c7dd415
Jinsu Park <dev.umijs@gmail.com>
2021-11-07 02:52:50 +0900

    test: add test

--- everybody_errors_test.go
+++ everybody_errors_test.go
@@ -12,8 +12,12 @@ func Test(t *testing.T){
 	t.Logf("%+v", GetRootStackError(withStacked()))
 	t.Log("-----------------------------------------------------")
 	t.Logf("%+v", GetRootStackError(withMessaged()))
-
-
+	t.Log("-----------------------------------------------------")
+	t.Logf("%+v", GetRootStackError(mixed1()))
+	t.Log("-----------------------------------------------------")
+	t.Logf("%+v", GetRootStackError(mixed2()))
+	t.Log("-----------------------------------------------------")
+	t.Logf("%+v", GetRootStackError(G()))
 }
 
 func Root()error {
@@ -21,13 +25,50 @@ func Root()error {
 }
 
 func wrapped() error {
-	return Wrap(Root(), "wrapped")
+	return Wrap(Wrap(Wrap(Root(), "wrapped1"), "wrapped2"), "wrapped3")
 }
 
 func withStacked() error{
-	return WithStack(Root())
+	return WithStack(WithStack(WithStack(Root())))
 }
 
 func withMessaged() error{
-	return WithMessage(Root(), "withMessaged")
+	return WithMessage(WithMessage(WithMessage(Root(), "withMessaged"), "withMessaged2"), "withMessaged3")
+}
+
+func mixed1() error{
+	return WithMessage(WithStack(Wrap(Root(), "wrap")), "withMessage")
+}
+
+func mixed2() error{
+	return Wrap(WithStack(WithMessage(Root(), "wrap")), "withMessage")
+}
+
+
+func A() error{
+	return New("A")
+}
+
+func B() error{
+	return WithStack(A())
+}
+
+func C() error {
+	return Wrap(B(), "")
+}
+
+func D() error{
+	return WithMessage(C(), "")
+}
+
+func E() error{
+	return WithMessage(D(), "")
+}
+
+func F() error{
+	return Wrap(E(), "")
+}
+
+func G() error{
+	return WithStack(F())
 }
